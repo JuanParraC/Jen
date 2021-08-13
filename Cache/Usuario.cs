5b9effed14d5e58e3a2d040bf5cf2117311bd51a
Juan Parra <juan.parra.c@gmail.com>
2021-08-13 07:21:09 -0400

    Carga inicial

---
+++ Cache/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Jen.Cache
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+
+    [Serializable]
+    public class Usuario : Dictionary<string, ConcurrentBag<Semilla>>
+    {
+
+    }
+}
